35c7fd78c3926388d0652d0523315d460de9ce0a
30035982 <30035982@hq.es.corp.leroymerlin.com>
2017-12-05 18:45:44 +0100

    Openshift env vars

--- server.js
+++ server.js
@@ -3,8 +3,32 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-const port = process.env.PORT || 3000
-const db_link = process.env.DBLINK || 'mongodb://sntmigrationpreuser:sntMigrationPrePwd@aeslmnomona01:27017,aeslmnomona02:27017,aeslmnomona03:27017/sntmigration?replicaSet=aeslmnomon'
+const db_link =  'mongodb://sntmigrationpreuser:sntMigrationPrePwd@aeslmnomona01:27017,aeslmnomona02:27017,aeslmnomona03:27017/sntmigration?replicaSet=aeslmnomon'
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+      mongoUser = process.env[mongoServiceName + '_USER']
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://'
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@'
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase
+  }
+}
+if (mongoURL == null) mongoURL = db_link
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
